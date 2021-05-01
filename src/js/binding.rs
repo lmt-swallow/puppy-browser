1d176bebc80ac9629dc9d726d57cfa238f1dc1d1
Takashi Yoneuchi <takashi.yoneuchi@shift-js.info>
2021-05-02 07:26:49 +0900

    :construction: separate binding

--- src/js/binding.rs
+++ src/js/binding.rs
@@ -1,29 +1,45 @@
+mod console;
 use rusty_v8 as v8;
 
-pub fn initialize_context<'s>(scope: &mut v8::HandleScope<'s, ()>) -> v8::Local<'s, v8::Context> {
-    let scope = &mut v8::EscapableHandleScope::new(scope);
+/// `initialize_context` takes a HandleScope to `v8::Isolate` object and returns a new HandleScope to newly created `v8::Context`.
+pub fn initialize_context<'s>(
+    isolate_scope: &mut v8::HandleScope<'s, ()>,
+) -> v8::Local<'s, v8::Context> {
+    let scope = &mut v8::EscapableHandleScope::new(isolate_scope);
+
+    // create context
     let context = v8::Context::new(scope);
 
-    // TODO: initialization
-    // let template = v8::ObjectTemplate::new(isolate_handle_scope);
-    // template.set(
-    //     v8::String::new(isolate_handle_scope, "log").unwrap().into(),
-    //     v8::FunctionTemplate::new(
-    //         isolate_handle_scope,
-    //         |scope: &mut v8::HandleScope,
-    //          args: v8::FunctionCallbackArguments,
-    //          mut _retval: v8::ReturnValue| {
-    //             let message = args
-    //                 .get(0)
-    //                 .to_string(scope)
-    //                 .unwrap()
-    //                 .to_rust_string_lossy(scope);
+    // get global proxy object
+    let global = context.global(scope);
 
-    //             println!("Logged: {}", message);
-    //         },
-    //     )
-    //     .into(),
-    // );
+    // bind `console` object
+    let scope = &mut v8::ContextScope::new(scope, context);
+    console::initialize_console(scope, global);
 
+    // return with a handle to newly created v8::Context
     scope.escape(context)
+}
+
+pub fn create_object_under<'s>(
+    scope: &mut v8::HandleScope<'s>,
+    target: v8::Local<v8::Object>,
+    name: &'static str,
+) -> v8::Local<'s, v8::Object> {
+    let key = v8::String::new(scope, name).unwrap();
+    let value = v8::Object::new(scope);
+    target.set(scope, key.into(), value.into());
+    value
+}
+
+pub fn set_function_to(
+    scope: &mut v8::HandleScope<'_>,
+    target: v8::Local<v8::Object>,
+    name: &'static str,
+    callback: impl v8::MapFnTo<v8::FunctionCallback>,
+) {
+    let key = v8::String::new(scope, name).unwrap();
+    let tmpl = v8::FunctionTemplate::new(scope, callback);
+    let val = tmpl.get_function(scope).unwrap();
+    target.set(scope, key.into(), val.into());
 }
---
+++ src/js/binding/console.rs
@@ -0,0 +1,28 @@
+use log::info;
+use rusty_v8 as v8;
+
+use super::{create_object_under, set_function_to};
+
+pub fn initialize_console<'s>(
+    context_scope: &mut v8::ContextScope<'s, v8::EscapableHandleScope>,
+    global: v8::Local<v8::Object>,
+) -> v8::Local<'s, v8::Object> {
+    let console = create_object_under(context_scope, global, "console");
+    set_function_to(
+        context_scope,
+        console,
+        "info",
+        |scope: &mut v8::HandleScope,
+         args: v8::FunctionCallbackArguments,
+         mut _retval: v8::ReturnValue| {
+            info!(
+                "{}",
+                args.get(0)
+                    .to_string(scope)
+                    .unwrap()
+                    .to_rust_string_lossy(scope)
+            );
+        },
+    );
+    console
+}
